07e536b9d1ce467595ebb1a96d07dd16a6045c58
Jared Lumpe <mjlumpe@gmail.com>
2018-01-04 18:35:33 -0700

    setup.py script

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""Setuptools installation script for nbconvert-terminal package."""
+
+import os
+from setuptools import setup, find_packages
+
+
+# Directory of script
+root_dir = os.path.dirname(__file__)
+
+
+# Read readme file for long description
+with open(os.path.join(root_dir, 'README.md')) as fobj:
+	long_description = fobj.read()
+
+
+VERSION = '0.0.1'
+
+
+setup(
+	name='nbconvert-terminal',
+	version=VERSION,
+	description='Jupyter nbconvert extension for viewing notebooks in the terminal.',
+	long_description=long_description,
+	author='Jared Lumpe',
+	url='https://github.com/jlumpe/nbconvert-terminal',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=[
+		'nbconvert~=5.3',
+		'pygments~=2.2',
+	],
+	entry_points={
+		'nbconvert.exporters': [
+			'terminal = nbconvert_terminal:TerminalExporter',
+		],
+	},
+	include_package_data=True,
+)
